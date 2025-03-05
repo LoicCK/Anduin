7a735fe7582fb952c1a53704a43d81bccfc68694
algorythmTTV <loic.chauveau.kson@gmail.com>
2025-03-05 21:14:26 +0100

    added

---
+++ core/src/main/java/io/github/algorythmTTV/TGAA/TitleScreen.java
@@ -0,0 +1,76 @@
+package io.github.algorythmTTV.TGAA;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.Gdx;
+
+
+/** First screen of the application. Displayed after the application is created. */
+public class TitleScreen implements Screen {
+    final TGAA game;
+    Texture bgTexture;
+    private Stage stage;
+    private TextButton startButton;
+    private Skin skin;
+
+
+    public TitleScreen(TGAA game) {
+        this.game = game;
+        bgTexture = new Texture("TitleScreenBg.png");
+    }
+
+    @Override
+    public void show() {
+        // Prepare your screen here.
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.BLACK);
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+        game.batch.begin();
+
+        float worldWidth = game.viewport.getWorldWidth();
+        float worldHeight = game.viewport.getWorldHeight();
+
+        game.batch.draw(bgTexture, 0, 0, worldWidth, worldHeight);
+
+        game.font.draw(game.batch, "TGAA - The Great Adventures of Anduin", 10, 5);
+
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        game.viewport.update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void hide() {
+        // This method is called when another screen replaces this one.
+    }
+
+    @Override
+    public void dispose() {
+        bgTexture.dispose();
+    }
+}
